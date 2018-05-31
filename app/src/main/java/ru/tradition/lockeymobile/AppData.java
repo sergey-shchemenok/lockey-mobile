6a4be3a249794f695adb86c716b6cb77b80ebf79
sergey-aspect <sergey-aspect@yandex.ru>
2018-05-31 14:24:08 +0300

    release version

--- app/src/main/java/ru/tradition/lockeymobile/AppData.java
+++ app/src/main/java/ru/tradition/lockeymobile/AppData.java
@@ -27,7 +27,7 @@ import ru.tradition.lockeymobile.tabs.maptab.GeofencePolygon;
 
 public final class AppData {
 
-    public static final String VERSION = "0.9.27";
+    public static final String VERSION = "0.9.28";
     //fab is added to notification activity
 
     public static MainActivity mainActivity;

12f22bb110fb01f47ead181543c624d7e3ed3f03
sergey-aspect <sergey-aspect@yandex.ru>
2018-06-11 22:42:29 +0300

    before import

--- app/src/main/java/ru/tradition/lockeymobile/AssetActivity.java
+++ app/src/main/java/ru/tradition/lockeymobile/AssetActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.PorterDuff;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -180,7 +181,8 @@ public class AssetActivity extends AppCompatActivity {
                 mCommand = CHOOSE_COMMAND; // Unknown
             }
         });
-        mCommandSpinner.getBackground().setColorFilter(getResources().getColor(R.color.checkboxColor), PorterDuff.Mode.SRC_ATOP);
+        mCommandSpinner.getBackground().setColorFilter(ContextCompat.getColor(this, R.color.checkboxColor), PorterDuff.Mode.SRC_ATOP);
+//        mCommandSpinner.getBackground().setColorFilter(getResources().getColor(R.color.checkboxColor), PorterDuff.Mode.SRC_ATOP); deprecated. Unfortunately
 
         builder.setTitle("Отправка команд")
 //                .setMessage(R.string.user_agreement_text)
